afc06a6a606fb565b12648e2c1b5f974d5f6c639
Shashank Sharma <shashanksharma.er104@gmail.com>
2025-01-02 20:11:03 +0530

    Update test class name and added new endpoints interface

--- src/test/java/config/VideoGameConfig.java
+++ src/test/java/config/VideoGameConfig.java
@@ -5,17 +5,16 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import org.junit.BeforeClass;
 
-public class TestConfig {
+public class VideoGameConfig {
 
     @BeforeClass
     public static void setup() {
-        RestAssured.baseURI = "https://www.videogamedb.uk/";
-        RestAssured.basePath = "/api/v2";
-        RestAssured.port = 443; //optional if it's https
 
         //Inside setup() method
         RestAssured.requestSpecification = new RequestSpecBuilder()
-                .setContentType("application/xml")
+                .setBaseUri("https://www.videogamedb.uk/")
+                .setBasePath("/api/v2")
+                .setContentType("application/json")
                 //.setAccept("application/json")
                 .addHeader("Accept", "application/json") //use addHeader method to define explicitly
                 .build();
---
+++ src/test/java/config/VideoGameEndpoints.java
@@ -0,0 +1,8 @@
+package config;
+
+public interface VideoGameEndpoints {
+
+    public String ALL_VIDEO_GAME = "/videogame";
+    public String SINGLE_VIDEO_GAME = "/videogame/{videoGameId}";
+
+}
