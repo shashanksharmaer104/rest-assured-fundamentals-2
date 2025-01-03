7451dde07150811d1061325cc269939a6bc32796
Shashank Sharma <shashanksharma.er104@gmail.com>
2025-01-03 19:23:18 +0530

    Added PUT test cases

--- src/test/java/VideoGameTests.java
+++ src/test/java/VideoGameTests.java
@@ -6,6 +6,23 @@ import static io.restassured.RestAssured.*;
 
 public class VideoGameTests extends VideoGameConfig {
 
+    String requestGameJSONBody = "{\n" +
+            "  \"category\": \"Platform\",\n" +
+            "  \"name\": \"Transformers\",\n" +
+            "  \"rating\": \"Mature 123\",\n" +
+            "  \"releaseDate\": \"2012-05-04\",\n" +
+            "  \"reviewScore\": 85\n" +
+            "}";
+
+    String requestGameXMLBody = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<VideoGameRequest>\n" +
+            "\t<category>Platform</category>\n" +
+            "\t<name>Tekken 8</name>\n" +
+            "\t<rating>Mature</rating>\n" +
+            "\t<releaseDate>2024-08-04</releaseDate>\n" +
+            "\t<reviewScore>90</reviewScore>\n" +
+            "</VideoGameRequest>";
+
     @Test
     public void getAllVideoGames() {
         given()
@@ -16,15 +33,6 @@ public class VideoGameTests extends VideoGameConfig {
 
     @Test
     public void createNewGameByJSON() {
-
-        String requestGameJSONBody = "{\n" +
-                "  \"category\": \"Platform\",\n" +
-                "  \"name\": \"Transformers\",\n" +
-                "  \"rating\": \"Mature 123\",\n" +
-                "  \"releaseDate\": \"2012-05-04\",\n" +
-                "  \"reviewScore\": 85\n" +
-                "}";
-
         given()
                 .body(requestGameJSONBody)
         .when()
@@ -34,7 +42,6 @@ public class VideoGameTests extends VideoGameConfig {
 
     @Test
     public void createGameByXML() {
-
         String requestGameXMLBody = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
                 "<VideoGameRequest>\n" +
                 "\t<category>Platform</category>\n" +
@@ -45,11 +52,31 @@ public class VideoGameTests extends VideoGameConfig {
                 "</VideoGameRequest>";
 
         given()
-                .contentType("application/xml")
-                .accept("application/xml")
+                .contentType("application/xml") //to specify explicitly
+                .accept("application/xml") //to specify explicitly
                 .body(requestGameXMLBody)
         .when()
                 .post(VideoGameEndpoints.ALL_VIDEO_GAME)
+        .then();
+    }
+
+    @Test
+    public void updateGameByJSON() {
+        given()
+                .body(requestGameJSONBody)
+        .when()
+                .put("/videogame/2")
+        .then();
+    }
+
+    @Test
+    public void updateGameByXML() {
+        given()
+                .contentType("application/xml") //to specify explicitly
+                .accept("application/xml") //to specify explicitly
+                .body(requestGameXMLBody)
+        .when()
+                .put("/videogame/6")
         .then();
     }
 
