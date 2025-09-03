792d851260e8408c7271e55ccefd7b9661db5114
DESKTOP-FFNFBNB\shash <shashanksharma.er104@gmail.com>
2025-09-03 11:01:34 +0530

    Initial code setup and one test case

---
+++ src/test/java/MyFirstTestClass.java
@@ -0,0 +1,15 @@
+import config.TestConfig;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class MyFirstTestClass extends TestConfig {
+
+    @Test
+    public void myFirstTest() {
+        given().log().all()
+                .when().get("/videogame")
+                .then();
+    }
+
+}
---
+++ src/test/java/config/TestConfig.java
@@ -0,0 +1,15 @@
+package config;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+public class TestConfig {
+
+    @BeforeClass
+    public static void setup() {
+        RestAssured.baseURI = "https://videogamedb.uk/";
+        RestAssured.basePath = "/api/v2/";
+        RestAssured.port = 443;
+    }
+
+}
